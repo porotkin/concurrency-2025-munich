f35b88cfe264bb3f52ceeca2ebed52f486978022
porotkin <preload737773@gmail.com>
2025-07-23 17:48:59 +0300

    Day 2. `CountersSnapshot`

--- src/day2/CountersSnapshot.kt
+++ src/day2/CountersSnapshot.kt
@@ -1,6 +1,6 @@
 package day2
 
-import java.util.concurrent.atomic.*
+import java.util.concurrent.atomic.AtomicLong
 
 class CountersSnapshot {
     val counter1 = AtomicLong(0)
@@ -12,7 +12,18 @@ class CountersSnapshot {
     fun incrementCounter3() = counter3.getAndIncrement()
 
     fun countersSnapshot(): Triple<Long, Long, Long> {
-        // TODO: make me atomic using the double-collect technique.
-        return Triple(counter1.get(), counter2.get(), counter3.get())
+        while (true) {
+            val counter1 = this.counter1.get()
+            val counter2 = this.counter2.get()
+            val counter3 = this.counter3.get()
+
+            if (counter1 != this.counter1.get() ||
+                counter2 != this.counter2.get()
+            ) {
+                continue
+            }
+
+            return Triple(counter1, counter2, counter3)
+        }
     }
 }

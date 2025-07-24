113b48bf4e25b2471738d9ac0121fc5806082518
porotkin <preload737773@gmail.com>
2025-07-24 14:52:56 +0300

    Day 3. `AtomicArrayWithCAS2OnLockedState`

--- src/day3/AtomicArrayWithCAS2OnLockedState.kt
+++ src/day3/AtomicArrayWithCAS2OnLockedState.kt
@@ -1,7 +1,6 @@
 package day3
 
-import java.util.concurrent.atomic.*
-import java.util.concurrent.locks.*
+import java.util.concurrent.atomic.AtomicReferenceArray
 
 // This implementation never stores `null` values.
 class AtomicArrayWithCAS2OnLockedState<E : Any>(size: Int, initialValue: E) {
@@ -15,28 +14,63 @@ class AtomicArrayWithCAS2OnLockedState<E : Any>(size: Int, initialValue: E) {
     }
 
     fun get(index: Int): E {
-        // TODO: Cover the case when the cell state is LOCKED.
-        return array[index] as E
+        while (true) {
+            val value = array[index]
+            if (value === LOCKED) continue
+            @Suppress("UNCHECKED_CAST")
+            return value as E
+        }
     }
 
     fun cas2(
         index1: Int, expected1: E, update1: E,
-        index2: Int, expected2: E, update2: E
+        index2: Int, expected2: E, update2: E,
     ): Boolean {
         require(index1 != index2) { "The indices should be different" }
-        // TODO: Make me thread-safe by "locking" the cells
-        // TODO: via atomically changing their states to LOCKED.
-        if (array[index1] === expected1 && array[index2] === expected2) {
-            array.set(index1, update1)
-            array.set(index2, update2)
-            return true
-        } else {
+
+        val (pair1, pair2) = arrayOf(
+            index1 to expected1,
+            index2 to expected2,
+        ).sortedBy { it.first }
+
+        val (idx1, exp1) = pair1
+        val (idx2, exp2) = pair2
+
+        if (!lockCell(idx1, exp1)) {
             return false
+        }
+
+        if (!lockCell(idx2, exp2)) {
+            array[idx1] = exp1 // rollback
+            return false
+        }
+
+        array[index1] = update1
+        array[index2] = update2
+
+        return true
+    }
+
+    private fun lockCell(index: Int, expected: E): Boolean {
+        while (true) {
+            val value = array[index]
+            when (value) {
+                expected -> {
+                    if (array.compareAndSet(index, expected, LOCKED)) {
+                        return true
+                    }
+                }
+
+                LOCKED -> {
+                    continue
+                }
+
+                else -> {
+                    return false
+                }
+            }
         }
     }
 }
 
-// TODO: Store me in `a` to indicate that the reference is "locked".
-// TODO: Other operations should wait in an active loop until the
-// TODO: value changes.
 private val LOCKED = "Locked"

641d520905628f3f6d36941780bf09401a63df0d
porotkin <preload737773@gmail.com>
2025-07-24 14:52:40 +0300

    Day 3. `AtomicArrayWithCas2OnReentrantLock`

--- src/day3/AtomicArrayWithCas2OnReentrantLock.kt
+++ src/day3/AtomicArrayWithCas2OnReentrantLock.kt
@@ -1,13 +1,12 @@
 package day3
 
-import java.util.concurrent.atomic.*
-import java.util.concurrent.locks.*
+import java.util.concurrent.atomic.AtomicReferenceArray
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 // This implementation never stores `null` values.
 class AtomicArrayWithCas2OnReentrantLock<E : Any>(size: Int, initialValue: E) {
     private val array = AtomicReferenceArray<E>(size)
-
-    // TODO: Use these locks to protect the array cells.
     private val locks = Array(size) { ReentrantLock() }
 
     init {
@@ -18,23 +17,35 @@ class AtomicArrayWithCas2OnReentrantLock<E : Any>(size: Int, initialValue: E) {
     }
 
     fun get(index: Int): E {
-        // TODO: Guard this function with the cell lock.
-        return array[index]
+        locks[index].withLock {
+            return array[index]
+        }
     }
 
     fun cas2(
         index1: Int, expected1: E, update1: E,
-        index2: Int, expected2: E, update2: E
+        index2: Int, expected2: E, update2: E,
     ): Boolean {
         require(index1 != index2) { "The indices should be different" }
-        // TODO: Guard this function with the cell locks
-        // TODO: following the fine-grained locking approach.
-        if (array[index1] === expected1 && array[index2] === expected2) {
+
+        val (pair1, pair2) = arrayOf(
+            index1 to expected1,
+            index2 to expected2,
+        ).sortedBy { it.first }
+
+        val (idx1, exp1) = pair1
+        val (idx2, exp2) = pair2
+
+        locks[idx1].withLock {
+            if (array[idx1] != exp1) return false
+        }
+
+        locks[idx2].withLock {
+            if (array[idx2] != exp2) return false
+
             array.set(index1, update1)
             array.set(index2, update2)
             return true
-        } else {
-            return false
         }
     }
 }
